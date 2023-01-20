7a4fcd206ce2c5385fe95a8adcdf2873457c32b6
Andrey <andreytelushko@gmail.com>
2023-01-20 23:20:16 +0300

    lesson 36

--- HomeWork5/Program.cs
+++ HomeWork5/Program.cs
@@ -11,25 +11,27 @@ namespace HomeWork5
         static void Main(string[] args)
 
         {
+            //Задача 36: Задайте одномерный массив, заполненный случайными числами.
+            //Найдите сумму элементов, стоящих на нечётных позициях.
+
             Console.WriteLine("Введите размер массива");
             int size = Convert.ToInt32(Console.ReadLine());
             int[] numbers = new int[size];
             FillArrayRandomNumbers(numbers);
             Console.WriteLine("Вот наш массив: ");
             PrintArray(numbers);
-            int count = 0;
+            int sum = 0;
 
-            for (int z = 0; z < numbers.Length; z++)
-                if (numbers[z] % 2 == 0)
-                    count++;
+            for (int z = 0; z < numbers.Length; z += 2)
+                sum = sum + numbers[z];
 
-            Console.WriteLine($"всего {numbers.Length} чисел, {count} из них чётные");
+            Console.WriteLine($"всего {numbers.Length} чисел, сумма элементов на нечётных позициях = {sum}");
 
             void FillArrayRandomNumbers(int[] Numbers)
             {
                 for (int i = 0; i < numbers.Length; i++)
                 {
-                    numbers[i] = new Random().Next(100, 1000);
+                    numbers[i] = new Random().Next(1, 10);
                 }
             }
             void PrintArray(int[] Numbers)
@@ -43,5 +45,8 @@ namespace HomeWork5
                 Console.WriteLine();
             }
         }
+
+
+
     }
 }
